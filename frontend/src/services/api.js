13a7492a7db86b8f679e1ee55fa51192372033f3
rahul19112007-dev <rahul19112007@gmail.com>
2026-06-15 18:48:01 +0530

    Production backend configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://eduscorex-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
